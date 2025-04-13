a6853b49a26fec995b1dae2bea69208c7d6e00ef
madza-dell <mr.ghasemi.1375@gmail.com>
2025-04-13 21:22:04 +0330

    add dynamic path that can change by id

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { Routes,Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<h2>Home Page</h2>}/>
-      <Route path="/add" element={<h2>Add Post</h2>}/>
+      <Route path="/" element={<h2>Home Page</h2>} />
+      <Route path="/add" element={<h2>Add Post</h2>} />
       <Route path="/:id">
-      
+        <Route index element={<h2>View Post</h2>} />
+        <Route path="edit" element={<h2>Edit Post</h2>} />
       </Route>
-
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
