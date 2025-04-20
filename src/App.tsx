68ae8ea4bc2d562b2374f5eab1afed14bce1f2b0
madza <mr.ghasemi.1375@gmail.com>
2025-04-20 17:16:58 +0330

    use useLocalStorage custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,38 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import AddPost from "./components/AddPost";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
-export type Post ={
-  id: string
-}& PostData
+export type RawPost = {
+  id: string;
+} & PostData;
 
-export type PostData={
-  title: string
-  markdown: string
-  tags:Tag[]
-}
+export type RawPostData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
 
-export type Tag ={
-  label: string
-  id: string
-}
+export type PostData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  label: string;
+  id: string;
+};
 
 function App() {
+  const [posts, setPosts] = useLocalStorage<RawPost[]>("POSTS", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h2>Home Page</h2>} />
-        <Route path="/add" element={<AddPost/>} />
+        <Route path="/add" element={<AddPost />} />
         <Route path="/:id">
           <Route index element={<h2>View Post</h2>} />
           <Route path="edit" element={<h2>Edit Post</h2>} />
